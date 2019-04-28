13866cccb3f4da3cfe547f1651ecae84fe70f0e6
soft-pro-dev <gtiny113@gmail.com>
2019-04-27 21:58:16 -0700

    Completed monitoring calendar from backend

--- App/Redux/DailyRedux.js
+++ App/Redux/DailyRedux.js
@@ -1,62 +1,77 @@
-import {createActions, createReducer} from 'reduxsauce';
+import { createActions, createReducer } from 'reduxsauce';
 import Immutable from 'seamless-immutable';
 
-const {Types, Creators} = createActions({
-    postSuccess: ['postSuccess'],
-    postFail: ['postFail'],
-    fetchDaily: ['fetchDaily'],
-    fetchMood: ['fetchMood'],
-    fetchMoodFail: ['fetchMoodFail'],
-    fetchMealMenu: ['fetchMealMenu'],
-    fetchCarePlan: ['fetchCarePlan']
+
+const { Types, Creators } = createActions({
+	postSuccess: ['postSuccess'],
+	postFail: ['postFail'],
+	fetchDaily: ['fetchDaily'],
+	fetchMood: ['fetchMood'],
+	fetchMoodFail: ['fetchMoodFail'],
+	fetchMealMenu: ['fetchMealMenu'],
+	fetchCalendar: ['calendar'],
+	fetchCalendarFail: ['err'],
+	fetchCarePlan: ['fetchCarePlan'],
 });
 
 export const DailyRedux = Types;
 export default Creators;
 
 export const INITIAL_STATE = Immutable({
-    moods: {},
-    menus: {},
-    careplan: {},
-    fetching: false,
-    results: {},
-    error: null
+	moods: {},
+	menus: {},
+	careplan: {},
+	fetching: false,
+	results: {},
+	error: null,
+	calendar: []
 });
 
 export const dataObj = (state, payload) => {
-    return state.merge({fetching: true, results: payload.postSuccess});
+	return state.merge({ fetching: true, results: payload.postSuccess });
 };
 
 export const dataObjFail = (state, payload) => {
-    return state.merge({fetching: true, error: payload});
+	return state.merge({ fetching: true, error: payload });
 };
 
 export const fetchDaily = (state) => {
-    return state.merge({fetching: false});
+	return state.merge({ fetching: false });
 };
 
 export const fetchMood = (state, payload) => {
-    return state.merge({moods: payload.fetchMood});
+	return state.merge({ moods: payload.fetchMood });
 };
 
 export const fetchMoodFail = (state) => {
-    return state.merge({moods: []});
+	return state.merge({ moods: [] });
 };
 
 export const fetchMealMenu = (state, payload) => {
-    return state.merge({menus: payload.fetchMealMenu});
+	return state.merge({ menus: payload.fetchMealMenu });
 };
 
 export const fetchCarePlan = (state, payload) => {
-    return state.merge({careplan: payload.fetchCarePlan});
+	return state.merge({ careplan: payload.fetchCarePlan });
+};
+
+export const fetchCalendar = (state, payload) => {
+	console.log("eeee" + JSON.stringify(payload))
+	return state.merge({ calendar: payload.calendar });
+};
+
+export const fetchCalendarFail = (state, payload) => {
+	return state.merge({ calendar: [] });
 };
 
 export const reducer = createReducer(INITIAL_STATE, {
-    [DailyRedux.POST_SUCCESS]: dataObj,
-    [DailyRedux.POST_FAIL]: dataObjFail,
-    [DailyRedux.FETCH_DAILY]: fetchDaily,
-    [DailyRedux.FETCH_MOOD]: fetchMood,
-    [DailyRedux.FETCH_MOOD_FAIL]: fetchMoodFail,
-    [DailyRedux.FETCH_MEAL_MENU]: fetchMealMenu,
-    [DailyRedux.FETCH_CARE_PLAN]: fetchCarePlan
+	[DailyRedux.POST_SUCCESS]: dataObj,
+	[DailyRedux.POST_FAIL]: dataObjFail,
+	[DailyRedux.FETCH_DAILY]: fetchDaily,
+	[DailyRedux.FETCH_MOOD]: fetchMood,
+	[DailyRedux.FETCH_CALENDAR]: fetchCalendar,
+	[DailyRedux.FETCH_CALENDAR_FAIL]: fetchCalendarFail,
+	[DailyRedux.FETCH_MOOD_FAIL]: fetchMoodFail,
+	[DailyRedux.FETCH_MEAL_MENU]: fetchMealMenu,
+	[DailyRedux.FETCH_CARE_PLAN]: fetchCarePlan
 });
--- App/Screens/Login.js
+++ App/Screens/Login.js
@@ -1,134 +1,136 @@
-import React, {Component} from 'react';
-import {View, Text, Image, KeyboardAvoidingView, TextInput, TouchableOpacity, AsyncStorage, Alert} from 'react-native';
-import {connect} from 'react-redux';
-import {EventDispatcher} from "../Actions";
+import React, { Component } from 'react';
+import { View, Text, Image, KeyboardAvoidingView, TextInput, TouchableOpacity, AsyncStorage, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { EventDispatcher } from "../Actions";
 import Loading from '../Components/Loading';
 import Geolocation from '../Components/Geolocation';
 import styles from './Styles/Login';
-import {emptyString} from '../Common/Strings';
+import { emptyString } from '../Common/Strings';
 
 
 class Login extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            inputUser: emptyString,
-            inputPass: emptyString,
-            submit: false,
-            location: [null, null]
-        };
-        this.image = require('../Images/default/notepad-2.png');
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			inputUser: emptyString,
+			inputPass: emptyString,
+			submit: false,
+			location: [null, null]
+		};
+		this.image = require('../Images/default/notepad-2.png');
+	}
 
-    _userLogin () {
-        if (this.state.inputUser.length > 0 && this.state.inputPass.length > 0) {
-            this.setState({submit: true});
-            this.props.login({username: this.state.inputUser, password: this.state.inputPass})
-                .then(async (response) => {
-                    if (response.type === "LOGIN_SUCCESS") {
-                        let data = response.loginSuccess;
-                        if (data.error) {
-                            this.setState({submit: false});
-                            Alert.alert(
-                                'Invalid login details. Please try again.',
-                                null,
-                                [{text: 'Close'}]
-                            );
-                        } else {
-                            const {navigate} = this.props.navigation;
-                            
-                            await AsyncStorage.setItem('token', data.access);
-                            await AsyncStorage.setItem('refresh', data.refresh);
-                            this.props.fetchMood();
-                            this.props.fetchMealMenu();
-                            this.props.fetchCarePlan();
-                            this.setState({submit: false});
-                            if(data.su_id>=0){
-                                this.props.updateUser({id:data.su_id})
-                                navigate('HomeScreen');
-                            } else {
-                                let SU = await this.props.fetchServiceUser();
-                                this.setState({submit: false});
-                                if (SU.fetchUser && SU.fetchUser.length < 1) {
-                                    Alert.alert(
-                                        'Missing SU in this user.',
-                                        null,
-                                        [{text: 'Close'}]
-                                    );
-                                } else {
-                                    navigate('HomeScreen');
-                                }
-                            }
-                            
-                        }
-                    } else {
-                        this.setState({submit: false});
-                        Alert.alert(
-                            "An error happened while trying to log in. Please verify internet connection.",
-                            null,
-                            [{text: 'Close'}]
-                        );
-                    }
+	_userLogin() {
+		if (this.state.inputUser.length > 0 && this.state.inputPass.length > 0) {
+			this.setState({ submit: true });
+			this.props.login({ username: this.state.inputUser, password: this.state.inputPass })
+				.then(async (response) => {
+					if (response.type === "LOGIN_SUCCESS") {
+						let data = response.loginSuccess;
+						if (data.error) {
+							this.setState({ submit: false });
+							Alert.alert(
+								'Invalid login details. Please try again.',
+								null,
+								[{ text: 'Close' }]
+							);
+						} else {
+							const { navigate } = this.props.navigation;
 
-                });
-        }
-    }
+							await AsyncStorage.setItem('token', data.access);
+							await AsyncStorage.setItem('refresh', data.refresh);
+							this.props.fetchMood();
+							this.props.fetchMealMenu();
+							this.props.fetchCarePlan();
+							this.props.fetchCalendar()
+							this.setState({ submit: false });
+							if (data.su_id >= 0) {
+								this.props.updateUser({ id: data.su_id })
+								navigate('HomeScreen');
+							} else {
+								let SU = await this.props.fetchServiceUser();
+								this.setState({ submit: false });
+								if (SU.fetchUser && SU.fetchUser.length < 1) {
+									Alert.alert(
+										'Missing SU in this user.',
+										null,
+										[{ text: 'Close' }]
+									);
+								} else {
+									navigate('HomeScreen');
+								}
+							}
 
-  _getLocation = (loc) => {
-      this.setState({location: loc});
-  }
+						}
+					} else {
+						this.setState({ submit: false });
+						Alert.alert(
+							"An error happened while trying to log in. Please verify internet connection.",
+							null,
+							[{ text: 'Close' }]
+						);
+					}
 
-  render () {
-      return (
-          <KeyboardAvoidingView behavior="padding" style={styles.container}>
-              {this.state.submit &&
-          <Loading visible={this.state.submit} />
-              }
-              <Geolocation onLocation={this._getLocation} />
-              <View style={styles.logoContainer}>
-                  <Image style={styles.logo} source={this.image} />
-              </View>
-              <View style={styles.formContainer}>
-                  <TextInput
-                      placeholder="Username"
-                      placeholderTextColor="#CCCCCC"
-                      underlineColorAndroid="transparent"
-                      returnKeyType="next"
-                      autoCorrect={false}
-                      autoCapitalize="none"
-                      ref={(input) => this.inputUser = input}
-                      onSubmitEditing={() => this.inputPass.focus()}
-                      onChangeText={(text) => this.setState({inputUser: text})}
-                      value={this.state.inputUser}
-                      style={styles.input}
-                  />
-                  <TextInput
-                      placeholder="Password"
-                      placeholderTextColor="#CCCCCC"
-                      underlineColorAndroid="transparent"
-                      returnKeyType="go"
-                      secureTextEntry
-                      style={styles.input}
-                      ref={(input) => this.inputPass = input}
-                      onChangeText={(text) => this.setState({inputPass: text})}
-                      value={this.state.inputPass}
-                  />
-                  <TouchableOpacity style={styles.buttonContainer} onPress={() => this._userLogin()}>
-                      <Text style={styles.buttonText}>LOGIN</Text>
-                  </TouchableOpacity>
-              </View>
-          </KeyboardAvoidingView>
-      );
-  }
+				});
+		}
+	}
+
+	_getLocation = (loc) => {
+		this.setState({ location: loc });
+	}
+
+	render() {
+		return (
+			<KeyboardAvoidingView behavior="padding" style={styles.container}>
+				{this.state.submit &&
+					<Loading visible={this.state.submit} />
+				}
+				<Geolocation onLocation={this._getLocation} />
+				<View style={styles.logoContainer}>
+					<Image style={styles.logo} source={this.image} />
+				</View>
+				<View style={styles.formContainer}>
+					<TextInput
+						placeholder="Username"
+						placeholderTextColor="#CCCCCC"
+						underlineColorAndroid="transparent"
+						returnKeyType="next"
+						autoCorrect={false}
+						autoCapitalize="none"
+						ref={(input) => this.inputUser = input}
+						onSubmitEditing={() => this.inputPass.focus()}
+						onChangeText={(text) => this.setState({ inputUser: text })}
+						value={this.state.inputUser}
+						style={styles.input}
+					/>
+					<TextInput
+						placeholder="Password"
+						placeholderTextColor="#CCCCCC"
+						underlineColorAndroid="transparent"
+						returnKeyType="go"
+						secureTextEntry
+						style={styles.input}
+						ref={(input) => this.inputPass = input}
+						onChangeText={(text) => this.setState({ inputPass: text })}
+						value={this.state.inputPass}
+					/>
+					<TouchableOpacity style={styles.buttonContainer} onPress={() => this._userLogin()}>
+						<Text style={styles.buttonText}>LOGIN</Text>
+					</TouchableOpacity>
+				</View>
+			</KeyboardAvoidingView>
+		);
+	}
 }
 
 const dispatchToProps = (dispatch) => ({
-    updateUser: (user) => EventDispatcher.UpdateUser(user, dispatch),
-    login: (userData) => EventDispatcher.Login(userData, dispatch),
-    fetchMood: () => EventDispatcher.FetchMood(dispatch),
-    fetchMealMenu: () => EventDispatcher.FetchMealMenu(dispatch),
-    fetchServiceUser: () => EventDispatcher.FetchServiceUser(dispatch),
-    fetchCarePlan: () => EventDispatcher.FetchCarePlan(dispatch),
+	updateUser: (user) => EventDispatcher.UpdateUser(user, dispatch),
+	login: (userData) => EventDispatcher.Login(userData, dispatch),
+	fetchMood: () => EventDispatcher.FetchMood(dispatch),
+	fetchMealMenu: () => EventDispatcher.FetchMealMenu(dispatch),
+	fetchServiceUser: () => EventDispatcher.FetchServiceUser(dispatch),
+	fetchCarePlan: () => EventDispatcher.FetchCarePlan(dispatch),
+	fetchCalendar: () => EventDispatcher.FetchCalendar(dispatch),
 });
 
 export default connect(null, dispatchToProps)(Login);
